becc13291c1c32a882ed2bb48aa9798f4a327d2d
Héloïse Robin <heloise.robin.pro@gmail.com>
2025-01-07 11:00:02 +0100

    Functional-Scene-Changers-And-Teleportation-Anchors

--- Projet-Efficience-VR-Game/Assets/Scripts/SceneChanger.cs
+++ Projet-Efficience-VR-Game/Assets/Scripts/SceneChanger.cs
@@ -1,44 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class SceneChanger : MonoBehaviour
 {
     #region Attributes
-    public GameObject leftControllerGameObject; // Accepte un GameObject dans l'Inspector
-    private XRController leftController; // Composant XR Controller obtenu dynamiquement
-    public string targetSceneName; // Nom de la scène cible
+    // public string targetSceneName; // Nom de la scène cible
     #endregion
 
     #region Main
-    private void Start()
-    {
-        // Récupère le composant XR Controller depuis le GameObject
-        leftController = leftControllerGameObject.GetComponent<XRController>();
-    }
-
-    private void Update()
-    {
-        if (leftController != null &&
-            leftController.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out bool isPressed) && isPressed)
-        {
-            // Charge la scène
-            SceneManager.LoadScene(targetSceneName);
-        }
-    }
     #endregion
 
     #region Methods
-    private void OnTriggerEnter(Collider other)
+    // Méthode publique pour charger une scène par son nom
+    public void LoadSceneByName(string sceneName)
     {
-        // Vérifie que le ray interactor vise cette icône
-        if (other.CompareTag("Player"))
+        if (!string.IsNullOrEmpty(sceneName)) // Vérifie que le nom n'est pas vide
         {
-            // Active l'écoute du bouton "A"
-            Update();
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Le nom de la scène est vide ou invalide.");
         }
     }
     #endregion
